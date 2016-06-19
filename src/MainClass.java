88bdaf5bd1b3a98e8edcfa47627056c85ddf9ade
Varun Gupta <user@192.168.30.240>
2016-06-19 22:29:18 +0530

    First Commit

---
+++ src/MainClass.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+ System.out.println("Think One Number From 1 to 9 In Your Mind");
+ String a =scanner.nextLine();
+ 
+ System.out.println("Multiply That Number With 9");
+ String b =scanner.nextLine();
+ 
+ System.out.println("Sum Up The Digit Of The Calculated Number(s)");
+ String c =scanner.nextLine();
+ 
+ System.out.println("Minus The Calculated Number By 5");
+ String d =scanner.nextLine();
+ 
+ System.out.println("If Calculated Number is 1 assume it as 'A' , 2 as 'B' , and so on...");
+ String e =scanner.nextLine();
+ 
+ System.out.println("Now Think a Country Name start with that Letter");
+ String f =scanner.nextLine();
+ 
+ System.out.println("Now Think a Animal Name that starts with the Letter that is end of that Country Name");
+ String g =scanner.nextLine();
+ 
+ System.out.println("Then Think a Colour Name starts with the Letter that is end of that Animal Name");
+ String h =scanner.nextLine();
+ 
+ System.out.println("Machine is  Reading Your Mind........Press Enter");
+ String i =scanner.nextLine();
+ 
+ System.out.println("........................................Press Enter");
+ String k =scanner.nextLine();
+ 
+ System.out.println("You think, Color = Orange ,Animal = Kangaroo,Country = Denmark");
+ String j =scanner.nextLine();
+	}
+
+}
